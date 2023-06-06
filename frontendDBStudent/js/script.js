fa3853ea5e963fb15b85caed1735e4da0f0bba4f
HasanovDoc <hasanov326@mail.ru>
2023-06-06 06:21:02 +0400

    Рабочий fetch запрос

--- frontendDBStudent/js/script.js
+++ frontendDBStudent/js/script.js
@@ -1,23 +1,49 @@
-const btnSubmit = document.querySelector('.form__input');
+const btnSubmit = document.querySelector('.input-data-form');
+const url = 'http://127.0.0.1:5501';
 
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-            'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *client
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return await response.json(); // parses JSON response into native JavaScript objects
+
+
+let options = {
+    method: 'POST',
+    //body: JSON.stringify(data),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+
+async function postData(url) {
+
+    try {
+        const response = await fetch(url, options)
+        const data = await response.json()
+        console.log('Data: ', JSON.stringify(data));
+        btnSubmit.classList.remove('submit-btn');
+    } catch (e) {
+        console.log('Fuck:', e);
+
+    }
+
+    // const response = await fetch(url, {
+    //     body: JSON.stringify(data) // body data type must match "Content-Type" header
+    // });
+    // return await response.json(); // parses JSON response into native JavaScript objects
 }
 
-btnSubmit.addEventListener('click', () => {
 
+
+dataInputs = document.querySelectorAll('.control__input');
+postData(url)
+btnSubmit.addEventListener('submit', () => {
+    //console.log(dataInputs);
+    //postData('localhost:5500', dataInputs)
+    postData(url)
 });
+
+// const students = {
+//     firstName: '',
+//     secondName: '',
+//     id_speciality: 0,
+
+
+// }
