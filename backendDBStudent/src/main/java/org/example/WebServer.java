dbc1566fecb66fd159ac7d0c83c034eb7e225ac6
HasanovDoc <hasanov326@mail.ru>
2023-06-06 12:01:01 +0400

    Рабочий запрос и ответ(!выполняются try и catch)

--- backendDBStudent/src/main/java/org/example/WebServer.java
+++ backendDBStudent/src/main/java/org/example/WebServer.java
@@ -7,6 +7,14 @@ import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+
+
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -16,37 +24,46 @@ public class WebServer {
     public void startServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/data", new DataHandler());
-        server.createContext("/", new StaticFileHandler("frontendDBStudent"));
+        server.createContext("/", new StaticFileHandler("../frontendDBStudent"));
         server.setExecutor(null);
         server.start();
-
-        // Запуск фронтенда
-        String frontendPath = "frontendDBStudent";
-        String indexFile = frontendPath + "/index.html";
-        String os = System.getProperty("os.name").toLowerCase();
-        String command = "";
-
-        if (os.contains("win")) {
-            command = "cmd /c start " + indexFile;
-        } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
-            command = "xdg-open " + indexFile;
-        }
-
-        try {
-            Runtime.getRuntime().exec(command);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         System.out.println("Server is running on port 8000");
     }
 
     static class DataHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            // Обработка запросов
+            exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+            if (exchange.getRequestMethod().equalsIgnoreCase("POST")) {
+                String requestBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+                System.out.println("Received data: " + requestBody);
+
+                // Здесь вы можете добавить логику для обработки данных
+                // и записи их в таблицу на PostgreSQL
+
+                //String response = "Data received successfully";
+                String response = "{\"message\": \"Data received successfully\"}";
+                exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+                exchange.sendResponseHeaders(200, response.length());
+                OutputStream outputStream = exchange.getResponseBody();
+                outputStream.write(response.getBytes());
+                outputStream.close();
+            } else if (exchange.getRequestMethod().equalsIgnoreCase("OPTIONS")) {
+                exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+                exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, OPTIONS");
+                exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
+                exchange.sendResponseHeaders(204, -1);
+                exchange.close();
+            } else {
+                String response = "Invalid request method";
+                exchange.sendResponseHeaders(400, response.length());
+                OutputStream outputStream = exchange.getResponseBody();
+                outputStream.write(response.getBytes());
+                outputStream.close();
+            }
         }
     }
+
 
     static class StaticFileHandler implements HttpHandler {
         private String rootPath;
@@ -76,4 +93,18 @@ public class WebServer {
             }
         }
     }
+
+    static class OptionsHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
+            exchange.sendResponseHeaders(204, -1);
+            exchange.close();
+        }
+    }
+
 }
+
+
--- frontendDBStudent/js/script.js
+++ frontendDBStudent/js/script.js
@@ -1,45 +1,63 @@
-const btnSubmit = document.querySelector('.input-data-form');
-const url = 'http://127.0.0.1:5501';
+const formSubmit = document.querySelector('.input-data-form');
+const url = 'http://localhost:8000';
 
 
+let data = {
+    firstName: '',
+    secondName: '',
+    id_speciality: 0,
+    course: 0,
+    numGroup: 0
+};
 
 let options = {
     method: 'POST',
-    //body: JSON.stringify(data),
+    body: JSON.stringify(data),
     headers: {
         'Content-Type': 'application/json'
     }
 };
 
 
-async function postData(url) {
+async function postData(varUrl) {
 
     try {
-        const response = await fetch(url, options)
-        const data = await response.json()
-        console.log('Data: ', JSON.stringify(data));
-        btnSubmit.classList.remove('submit-btn');
+        const response = await fetch(varUrl, options)
+        const res = await response.json()
+        console.log('Response: ', res);
+        //btnSubmit.classList.remove('submit-btn');
     } catch (e) {
-        console.log('Fuck:', e);
+        console.log('ErrrroR:', e);
 
     }
-
-    // const response = await fetch(url, {
-    //     body: JSON.stringify(data) // body data type must match "Content-Type" header
-    // });
-    // return await response.json(); // parses JSON response into native JavaScript objects
 }
 
 
 
-dataInputs = document.querySelectorAll('.control__input');
-postData(url)
-btnSubmit.addEventListener('submit', () => {
-    //console.log(dataInputs);
-    //postData('localhost:5500', dataInputs)
+
+//postData(url)
+
+formSubmit.addEventListener('submit', () => {
     postData(url)
+    let dataInputs = document.querySelectorAll('.reg-student');
+    setStudent(dataInputs);
+
+    options.body = JSON.stringify(data);
+    console.log(options);
+
+    newUrl = url + '/data';
+    postData(newUrl)
+    console.log(newUrl);
+
 });
 
+function setStudent(inputValue) {
+    data.firstName = inputValue[0].value;
+    data.secondName = inputValue[1].value;
+    data.id_speciality = inputValue[2].value;
+    data.course = inputValue[3].value;
+    data.numGroup = inputValue[4].value;
+}
 // const students = {
 //     firstName: '',
 //     secondName: '',
