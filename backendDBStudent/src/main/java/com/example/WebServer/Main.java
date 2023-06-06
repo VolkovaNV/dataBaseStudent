efc02b72ac072388f4f7c586d20021d97636d1c8
HasanovDoc <hasanov326@mail.ru>
2023-06-06 14:32:59 +0400

    Java переделан под Maven, серв на SpringFramework

---
+++ backendDBStudent/src/main/java/com/example/WebServer/Main.java
@@ -0,0 +1,18 @@
+//package com.example.WebServer;
+//
+//import java.io.IOException;
+//
+//public class Main {
+////    public static void main(String[] args) {
+////        System.out.println("Hello world!");
+////    }
+//
+//    public static void main(String[] args) {
+//        WebServer webServer = new WebServer();
+//        try {
+//            webServer.startServer();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
---
+++ backendDBStudent/src/main/java/com/example/WebServer/WebServer.java
@@ -0,0 +1,28 @@
+package com.example.WebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class WebServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServer.class, args);
+	}
+
+	@CrossOrigin(origins = "*") // Разрешаем запросы с любого источника
+	@RestController
+	public static class DataController {
+
+		@PostMapping("/data")
+		public String postData(@RequestBody String data) {
+			System.out.println("Received data: " + data);
+
+			// Здесь вы можете добавить логику для обработки данных
+			// и записи их в таблицу на PostgreSQL
+
+			return "{\"message\": \"Data received successfully\"}";
+		}
+	}
+}
