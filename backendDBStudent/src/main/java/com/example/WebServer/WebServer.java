e5a9d399acfac5052675353eb79789ef3e67d996
HasanovDoc <hasanov326@mail.ru>
2023-06-09 01:49:13 +0400

    Изменен тип данных у класса addStudent

--- backendDBStudent/src/main/java/com/example/WebServer/WebServer.java
+++ backendDBStudent/src/main/java/com/example/WebServer/WebServer.java
@@ -1,6 +1,6 @@
 package com.example.WebServer;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,23 +10,25 @@ public class WebServer {
 	@RestController
 	public static class DataController {
 		@PostMapping("/addStudent")
-		public String addStudent(@RequestBody String data) {
-			System.out.println("Received data: " + data);
+		public Student addStudent(@RequestBody Student student) {
+			//System.out.println("Received data: " + student.getFirstName().getClass().getName());
 
 			// Здесь вы можете добавить логику для обработки данных
 			// и записи их в таблицу на PostgreSQL
 
-			return data;
+
+
+			return student;//new ResponseEntity<>(name, HttpStatus.OK);
 		}
 
 		@PostMapping("/addSpeciality")
-		public String addSubject(@RequestBody String data) {
-			System.out.println("Received Speciality: " + data);
+		public String addSubject(@RequestBody String subject) {
+			System.out.println("Received Speciality: " + subject);
 
 			// Здесь вы можете добавить логику для обработки данных
 			// и записи их в таблицу на PostgreSQL
 
-			return data;
+			return subject;
 		}
 	}
 }
